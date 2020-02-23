5cba271294b842590eef1f989c9c2a0bbcc9883c
MikeLeYeti <mikeleyeti@yahoo.fr>
2020-02-23 17:12:58 +0100

    Interface graphique sous flask

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// Code à compléter pour obtenir une case cliquable... Il faudrait utiliser deux boucle for ...
+const buttonElement = document.getElementById('00');
+
+buttonElement.addEventListener('click', function (event) {
+    document.location.href = "/jouer/0/0"
+});
