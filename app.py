e52d374ea430d59d27bf1fdb205f52beb3e8ac3a
Romain Casati <romain.casati@webati.fr>
2020-02-26 01:59:09 +0100

    Contributions discutées par e-mail le 25/02/2020 : - séparation du code flask et du code morpion - génération du code html du plateau avec une boucle - gestion des conventions plus modulaire qui permet d'en changer simplement - utilisation de variables globales au lieu de l'écriture dans un fichier

--- app.py
+++ app.py
@@ -1,144 +1,87 @@
-import json
-from random import choice
-
 from flask import Flask, render_template, redirect, request
+import morpion
+
 
 morpion_app = Flask(__name__)
+plateau, joueur = 2 * [None]
 
 
-## Conventions :
-# -1 = X
-# 0 = Vide
-# 1 = O
+# permet de passer une fonction à Jinja2
+@morpion_app.context_processor
+def utility_processor():
+    def pion(joueur):
+        return {morpion.X: u"❌",
+                morpion.Vide: u"?",
+                morpion.O: u"🔵"}[joueur]
 
-
-## Initialisation
-def initialisation(joueur, premier):
-    dico = {
-        "plateau": [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-        "joueur": joueur,  # 'X' ou 'O'
-        "suivant": premier,  # 'humain' ou 'ia'
-        "gagnant": False,
-        "nulle": False
-    }
-    with open('donnees.txt', 'w') as fichier:
-        json.dump(dico, fichier)
-
-
-def lecture_donnees():
-    with open('donnees.txt') as json_file:
-        dico = json.load(json_file)
-    plateau, joueur, suivant, gagnant, nulle = dico["plateau"], dico["joueur"], dico["suivant"], dico["gagnant"], dico[
-        "nulle"]
-    return plateau, joueur, suivant, gagnant, nulle
-
-
-def ecriture_donnees(plateau, joueur, suivant, gagnant, nulle):
-    dico = {
-        "plateau": plateau,
-        "joueur": joueur,
-        "suivant": suivant,
-        "gagnant": gagnant,
-        "nulle": nulle
-    }
-    with open('donnees.txt', 'w') as fichier:
-        json.dump(dico, fichier)
-
-
-def IA_niveau_0(plateau, joueur):
-    liste_coups = []
-    for i in range(len(plateau)):
-        for j in range(len(plateau[i])):
-            if plateau[i][j] == 0:
-                liste_coups.append((i, j))
-    i, j = choice(liste_coups)
-    if joueur == 'X':
-        plateau[i][j] = 1
-    else:
-        plateau[i][j] = -1
-    return plateau
-
-
-def test_fin_partie(plateau):
-    gagnant = False
-    nulle = False
-    # test lignes
-    for i in range(len(plateau)):
-        if plateau[i][0] == plateau[i][1] == plateau[i][2] and plateau[i][0] != 0:
-            gagnant = plateau[i][0]
-    # test colonnes
-    for i in range(len(plateau)):
-        if plateau[0][i] == plateau[1][i] == plateau[2][i] and plateau[0][i] != 0:
-            gagnant = plateau[0][i]
-    # test diagonales
-    if ((plateau[0][0] == plateau[1][1] == plateau[2][2]) or (plateau[0][2] == plateau[1][1] == plateau[2][0])) and \
-            plateau[1][1] != 0:
-        gagnant = plateau[1][1]
-    if sum([l.count(0) for l in plateau]) == 0:  # Compte le nombre de case = 0
-        if gagnant == False:
-            nulle = True
-    return gagnant, nulle
-
-
-# Tests de la fonction test_fin_partie
-assert test_fin_partie([[0, 0, 0], [0, 0, 0], [0, 0, 0]]) == (False, False)
-assert test_fin_partie([[1, 1, 1], [0, 0, 0], [0, 0, 0]]) == (1, False)
-assert test_fin_partie([[0, 1, 1], [-1, -1, -1], [0, 0, 0]]) == (-1, False)
-assert test_fin_partie([[0, 1, 1], [-1, 1, -1], [1, 0, 0]]) == (1, False)
-assert test_fin_partie([[0, 1, -1], [-1, 1, -1], [1, 0, -1]]) == (-1, False)
-assert test_fin_partie([[-1, 1, -1], [-1, 1, -1], [1, -1, 1]]) == (False, True)
+    def est_vide(ligne, colonne):
+        return morpion.est_vide(plateau, ligne, colonne)
+    return dict(pion=pion, est_vide=est_vide)
 
 
 @morpion_app.route('/', methods=['GET'])
-def nouvellePartie():
+def nouvelle_partie():
     return render_template('nouvelle_partie.html')
 
 
 @morpion_app.route('/', methods=['POST'])
-def debutJeu():
-    joueur = request.form['joueur']
-    premier = request.form['debut']
-    initialisation(joueur, premier)
+def debut_jeu():
+    global plateau, joueur, joueur_ia
+
+    plateau = morpion.nouveau_plateau()
+    joueur = getattr(morpion, request.form['joueur'])
+    if request.form['debut'] == "ia":
+        jouer_ia()
+
     return redirect('/jeu')
 
 
 @morpion_app.route('/jeu')
 def affichage():
-    plateau, joueur, suivant, gagnant, nulle = lecture_donnees()
-    gagnant, nulle = test_fin_partie(plateau)
-    if nulle or gagnant != False:
-        return render_template("fin-de-partie.html", gagnant=gagnant, nulle=nulle, joueur=joueur)
-    if suivant == 'ia':
-        plateau = IA_niveau_0(plateau, joueur)
-        suivant = 'humain'
-    gagnant, nulle = test_fin_partie(plateau)
-    if nulle or gagnant != False:
-        return render_template("fin-de-partie.html", gagnant=gagnant, nulle=nulle, joueur=joueur)
-    ecriture_donnees(plateau, joueur, suivant, gagnant, nulle)
     return render_template('jeu.html', plateau=plateau)
 
 
-@morpion_app.route('/jouer/<int:ligne>/<int:colonne>')
+def fin_partie(joueur_test):
+    if morpion.victoire(plateau, joueur_test):
+        return render_template("fin_de_partie.html",
+                               gagnant=joueur_test,
+                               nulle=False,
+                               joueur=joueur)
+    if morpion.est_plein(plateau):
+        return render_template("fin_de_partie.html",
+                               gagnant=joueur_test,
+                               nulle=True,
+                               joueur=joueur)
+
+
+def jouer_joueur(ligne, colonne):
+    morpion.jouer(plateau, joueur, ligne, colonne)
+    return fin_partie(joueur)
+
+
+def jouer_ia():
+    joueur_ia = morpion.autre_joueur(joueur)
+    morpion.jouer_ia_alea(plateau, joueur_ia)
+    return fin_partie(joueur_ia)
+
+
+@morpion_app.route('/jouer?l=<int:ligne>&c=<int:colonne>')
 def jouer(ligne, colonne):
-    plateau, joueur, suivant, gagnant, nulle = lecture_donnees()
-    # # A modifier : faire appel à une fonction jouer qui teste la case...
-    if suivant == 'humain':
-        if joueur == 'X':
-            plateau[ligne][colonne] = -1
-        else:
-            plateau[ligne][colonne] = 1
-        suivant = 'ia'
-    else:  # suivant == 'ia'
-        # fonction IA basée sur min/max pour remplacer IA aléatoire
-        print("A l'ordi de jouer")
-    ecriture_donnees(plateau, joueur, suivant, gagnant, nulle)
+    # on fait jouer le joueur
+    res = jouer_joueur(ligne, colonne)
+    if res is not None:
+        return res
+
+    # puis l'ia
+    res = jouer_ia()
+    if res is not None:
+        return res
+
     return redirect('/jeu')
 
 
 @morpion_app.route("/reset")
 def reset():
-    # plateau, joueur, suivant, gagnant, nulle = lecture_donnees()
-    # initialisation(joueur,suivant)
     return redirect('/')
 
 
---
+++ morpion.py
@@ -0,0 +1,100 @@
+from random import choice
+
+
+# convention pour le plateau
+X, Vide, O = -1, 0, 1
+
+
+def nouveau_plateau():
+    """
+    Retroune un plateau vide.
+    """
+    return [[Vide] * 3 for _ in range(3)]
+
+
+def autre_joueur(joueur):
+    """
+    TODO.
+    """
+    return O if joueur == X else O
+
+
+def est_vide(plateau, ligne, colonne):
+    """
+    TODO.
+    """
+    return plateau[ligne][colonne] == Vide
+
+
+def coups_possibles(plateau):
+    """
+    TODO.
+    """
+    return [(i, j) for i in range(3) for j in range(3)
+            if est_vide(plateau, i, j)]
+
+
+def jouer(plateau, joueur, ligne, colonne):
+    """
+    TODO.
+    """
+    plateau[ligne][colonne] = joueur
+
+
+def jouer_ia_alea(plateau, joueur):
+    """
+    TODO.
+    """
+    i, j = choice(coups_possibles(plateau))
+    jouer(plateau, joueur, i, j)
+
+
+def est_plein(plateau):
+    """
+    TODO.
+    """
+    return sum(l.count(Vide) for l in plateau) == 0
+
+
+def victoire_ligne(plateau, joueur):
+    """
+    TODO.
+    """
+    for ligne in plateau:
+        if all(c == joueur for c in ligne):
+            return True
+    return False
+
+
+def victoire_colonne(plateau, joueur):
+    """
+    TODO.
+    """
+    for j in range(3):
+        if all(plateau[i][j] == joueur for i in range(3)):
+            return True
+    return False
+
+
+def victoire_diagonale(plateau, joueur):
+    """
+    TODO.
+    """
+    if (all(plateau[2 - i][i] == joueur for i in range(3))
+            or all(plateau[i][i] == joueur for i in range(3))):
+            return True
+    return False
+
+
+def victoire(plateau, joueur):
+    """
+    TODO.
+    """
+    return (victoire_ligne(plateau, joueur)
+            or victoire_colonne(plateau, joueur)
+            or victoire_diagonale(plateau, joueur))
+
+
+if __name__ == "__main__":
+    # les tests sont à mettre ici
+    pass
